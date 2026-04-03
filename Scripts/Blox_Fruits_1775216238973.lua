e473e4f3d519bde6ad84443b58ba17ac9a7a38d4
alphacellsmart <aresduarte146@gmail.com>
2026-04-03 08:37:19 -0300

    publish: [SCRIPT] Blox Fruits — Redz Hub

---
+++ Scripts/Blox_Fruits_1775216238973.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/System_irontech.lua"))()({
+    HubName = "Redz Hub",
+    Script  = "https://raw.githubusercontent.com/PlockScripts/newredz/refs/heads/main/Remake-version.luau"
+})
