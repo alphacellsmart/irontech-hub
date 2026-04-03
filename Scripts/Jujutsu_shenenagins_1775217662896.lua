b838bb42d9cf8db3aaff6e7771fc15b5599ab41b
alphacellsmart <aresduarte146@gmail.com>
2026-04-03 09:01:03 -0300

    publish: Jujutsu shenenagins Script — TBO Hub

---
+++ Scripts/Jujutsu_shenenagins_1775217662896.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/System_irontech.lua"))()({
+    HubName = "TBO Hub",
+    Script  = "https://raw.githubusercontent.com/cool5013/TBO/main/TBOscript"
+})
