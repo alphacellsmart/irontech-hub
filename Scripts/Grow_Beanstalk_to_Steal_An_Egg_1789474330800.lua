2c179e3df46e7f56ddb64cc9bc4838352eedb738
alphacellsmart <aresduarte146@gmail.com>
2026-09-15 09:12:14 -0300

    publish: Small cheats menu

---
+++ Scripts/Grow_Beanstalk_to_Steal_An_Egg_1789474330800.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/System_irontech.lua"))()({
+    HubName = "IronTech",
+    Script  = "https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/Scripts/_raw_Grow_Beanstalk_to_Steal_An_Egg_1789474330800.lua"
+})
