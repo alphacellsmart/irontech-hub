c36390b01fc4c10b278f0c2c7870a011e5452828
alphacellsmart <aresduarte146@gmail.com>
2026-09-15 09:12:25 -0300

    publish: Keystone rainbow nametag

---
+++ Scripts/Script_hub_1789474343424.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/System_irontech.lua"))()({
+    HubName = "IronTech",
+    Script  = "https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/Scripts/_raw_Script_hub_1789474343424.lua"
+})
