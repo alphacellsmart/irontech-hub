fb4fa2c675d437d92abbf54ad26d8a9641c18f56
alphacellsmart <aresduarte146@gmail.com>
2026-09-21 08:30:42 -0300

    publish: ARASAKA HUB NO KEY, AUTO FARM

---
+++ Scripts/_S15___1_Speed_Keyboard_Escape___Candy___1789990239584.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/System_irontech.lua"))()({
+    HubName = "ARASAKA HUB NO KEY, AUTO FARM",
+    Script  = "https://raw.githubusercontent.com/alphacellsmart/irontech-hub/main/Scripts/_raw__S15___1_Speed_Keyboard_Escape___Candy___1789990239584.lua"
+})
